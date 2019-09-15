600f7e3d8f8d9f19710a798ee728bd49505b179c
Jakub Karolak <karolakjakub90@gmail.com>
2019-09-15 12:44:08 +0200

    adding input range and handling NaNs

--- ansta/src/components/PayRange.js
+++ ansta/src/components/PayRange.js
@@ -1,9 +1,15 @@
 import Slider from '@material-ui/core/Slider';
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function PayRange(props) {
 
-    const { minmax, payRangeSliderChange, value, test } = props
+    const { minmax, payRangeSliderChange, value } = props
+
+    const [inputValue, setInputValue] = useState(value)
+
+    // useEffect(() => {
+
+    // }, [inputValueMin])
 
     const labels = [
         {
@@ -22,7 +28,26 @@ export default function PayRange(props) {
 
     };
 
-    return (<div className='sliderWrapper'>
+    const handleInputChange = (event) => {
+
+        if(event.target.name === 'inputMin'){
+
+            const newValue = [event.target.value === '' ? 0 :parseInt(event.target.value), inputValue[1]]
+            console.log(newValue)
+
+            setInputValue(newValue)
+
+        }else if(event.target.name === 'inputMax'){
+
+            const newValue = [inputValue[0], event.target.value === '' ? 0 :parseInt(event.target.value)]
+
+            setInputValue(newValue)
+        }
+        // setInputValue(event.target.value)
+
+    }
+
+    return (<div className='sliderWrapper'>{console.log(inputValue)}
         <Slider
             min={minmax[0]}
             max={minmax[1]}
@@ -31,6 +56,17 @@ export default function PayRange(props) {
             aria-labelledby="range-slider"
             marks={labels}
             valueLabelDisplay="on"></Slider>
+        <input
+        value={inputValue[0]}
+        name='inputMin'
+        onChange={handleInputChange}
+        ></input>
+        -
+        <input
+        value={inputValue[1]}
+        name='inputMax'
+        onChange={handleInputChange}
+        ></input>
 
     </div>
     )
